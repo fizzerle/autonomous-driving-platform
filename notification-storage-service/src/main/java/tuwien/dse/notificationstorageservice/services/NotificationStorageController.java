b77f9e03fe8cdc6c68a3db2c95d1b24be194fced
fizzerle <thomas.schallert94@gmail.com>
2019-04-09 18:33:22 +0200

    added notification storage service and routing

---
+++ notification-storage-service/src/main/java/tuwien/dse/notificationstorageservice/services/NotificationStorageController.java
@@ -0,0 +1,25 @@
+package tuwien.dse.notificationstorageservice.services;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationStorageController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NotificationStorageController.class);
+
+    @GetMapping("/")
+    public String findAll() {
+        LOGGER.info("Event find all");
+        return "find All events yyyyyyyyyyyyyyyyyyyess";
+    }
+
+    @GetMapping("/test/")
+    public String test() {
+        LOGGER.info("test called");
+        return "test called";
+    }
+}
