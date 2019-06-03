0fadb43bb753b98ae38ac02a5a07e611684929f8
fizzerle <thomas.schallert94@gmail.com>
2019-06-03 23:38:06 +0200

    updated entity storage controller

--- entity-storage-service/src/main/java/tuwien/dse/entitystorageservice/rest/EntityStorageController.java
+++ entity-storage-service/src/main/java/tuwien/dse/entitystorageservice/rest/EntityStorageController.java
@@ -1,7 +1,6 @@
 package tuwien.dse.entitystorageservice.rest;
 
 
-import com.sun.xml.internal.ws.client.sei.ResponseBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
