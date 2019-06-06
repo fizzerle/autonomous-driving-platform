45b81e3ed59adc9f362de2d135e572481bc525fc
schokobaer <andreas.peter.fend@gmail.com>
2019-06-06 19:59:19 +0200

    Added test hystrix rest endpoint in entitystorage

--- entity-storage-service/src/main/java/tuwien/dse/entitystorageservice/rest/EntityStorageController.java
+++ entity-storage-service/src/main/java/tuwien/dse/entitystorageservice/rest/EntityStorageController.java
@@ -34,6 +34,14 @@ public class EntityStorageController {
         return "entitystorage test";
     }
 
+    @GetMapping("/entitystorage/endless")
+    public String endlesshystrixcall() throws InterruptedException {
+        LOGGER.info("Start of endless hystrix call");
+        Thread.sleep(10000);
+        LOGGER.info("End of endless hystrix call");
+        return "i finally processed your request";
+    }
+
 
     @GetMapping("/entitystorage/cars")
     public List<Car> getAll(@RequestParam(required = false) Optional<String> oem) {
