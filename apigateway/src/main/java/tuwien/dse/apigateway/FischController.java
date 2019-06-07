6037f3274ca09953f5213f6e10474ea186252fa2
schokobaer <andreasfend@msn.com>
2019-06-07 20:51:48 +0200

    Igel test

--- apigateway/src/main/java/tuwien/dse/apigateway/FischController.java
+++ apigateway/src/main/java/tuwien/dse/apigateway/FischController.java
@@ -1,7 +1,9 @@
 package tuwien.dse.apigateway;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +20,16 @@ public class FischController {
     @GetMapping("/dead")
     public String getDeadFisch() {
         return fischService.getDeadFisch();
+    }
+
+    @GetMapping("/igel/*")
+    public String getigel(HttpRequest req){
+        return req.getURI().toString();
+    }
+
+    @PostMapping("/igel/*")
+    public String postigel(HttpRequest req){
+        return req.getURI().toString();
     }
 
 }
