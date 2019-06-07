a18bc6b368397b53562694b53487725346b272d0
Thomas <thomas.schallert94@gmail.com>
2019-06-07 11:01:33 +0200

    deleted ingress and mongodb depyloment in kubernetes

--- kubernetes/execute-all.sh
+++ kubernetes/execute-all.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 kubectl replace --force -f mongo-secret.yaml
 kubectl replace --force -f mongo-configmap.yaml
-kubectl replace --force -f mongo-deployment.yaml
 kubectl replace --force -f entitystorageservice-deployment.yaml
 kubectl replace --force -f eventstorageservice-deployment.yaml
 kubectl replace --force -f notificationstorageservice-deployment.yaml
-kubectl replace --force -f ingress.yaml
 kubectl replace --force -f backendui-deployment.yaml
 kubectl replace --force -f swaggergateway-deployment.yaml
 kubectl replace --force -f apigateway-deployment.yaml
