34080f0f80feded9d8bf08624fd5858256208749
schokobaer <andreasfend@msn.com>
2019-06-08 22:42:14 +0200

    Added Redisservice to Notificationstorage

--- event-storage-service/src/main/java/tuwien/dse/eventstorageservice/services/RedisService.java
+++ event-storage-service/src/main/java/tuwien/dse/eventstorageservice/services/RedisService.java
@@ -1,4 +1,4 @@
-package tuwien.dse.entitystorageservice.service;
+package tuwien.dse.eventstorageservice.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
