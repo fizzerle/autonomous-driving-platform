823515e6c5f343373f92b536b21b0982980f2fc1
fizzerle <thomas.schallert94@gmail.com>
2019-06-08 23:54:04 +0200

    get uri the new way

--- apigateway/src/main/java/tuwien/dse/apigateway/FallbackRestController.java
+++ apigateway/src/main/java/tuwien/dse/apigateway/FallbackRestController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.MediaType;
@@ -11,7 +12,11 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.pattern.PathPattern;
 import tuwien.dse.apigateway.cache.RedisService;
+
+import java.util.Map;
 
 @RestController
 public class FallbackRestController {
@@ -22,12 +27,20 @@ public class FallbackRestController {
     private RedisService redisService;
 
     @RequestMapping(value = "/hystrixfallback",produces = MediaType.APPLICATION_JSON_VALUE)
-    public String fallback(ServerHttpRequest request) {
+    public String fallback(ServerWebExchange serverWebExchange) {
         LOGGER.warn("Circute Breaker");
         /*LOGGER.info("ORIGIN: {}", headers.getOrigin());
         LOGGER.info("LOCATION: {}", headers.getLocation());*/
-        LOGGER.info("URI: {}", request.getURI());
-        LOGGER.info("Request: ", request);
+
+        //Get the PathMatchInfo
+        PathPattern.PathMatchInfo pathMatchInfo = serverWebExchange.getAttribute(ServerWebExchangeUtils.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+
+        //Get the template variables
+        Map<String, String> urlTemplateVariables = pathMatchInfo.getUriVariables();
+
+        for (Map.Entry<String, String> entry : urlTemplateVariables.entrySet()) {
+            LOGGER.info(""+entry.getKey()+" " + entry.getValue());
+        }
         return redisService.getCache("/entitystorage/oem");
     }
 }
