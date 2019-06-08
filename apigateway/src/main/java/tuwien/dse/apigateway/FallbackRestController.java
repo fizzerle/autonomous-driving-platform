a09356036b9895437b96d8e06b548ead90fc4503
schokobaer <andreasfend@msn.com>
2019-06-08 23:32:10 +0200

    ServerHttpRequest injected into hystrix fallback

--- apigateway/src/main/java/tuwien/dse/apigateway/FallbackRestController.java
+++ apigateway/src/main/java/tuwien/dse/apigateway/FallbackRestController.java
@@ -4,8 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tuwien.dse.apigateway.cache.RedisService;
@@ -19,8 +22,12 @@ public class FallbackRestController {
     private RedisService redisService;
 
     @RequestMapping(value = "/hystrixfallback",produces = MediaType.APPLICATION_JSON_VALUE)
-    public String fallback() {
+    public String fallback(ServerHttpRequest request) {
         LOGGER.warn("Circute Breaker");
+        /*LOGGER.info("ORIGIN: {}", headers.getOrigin());
+        LOGGER.info("LOCATION: {}", headers.getLocation());*/
+        LOGGER.info("URI: {}", request.getURI());
+        LOGGER.info("Request: ", request);
         return redisService.getCache("/entitystorage/oem");
     }
 }
