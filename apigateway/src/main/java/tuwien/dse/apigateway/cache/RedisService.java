113c329469244818174ac7ca3233a75c023d8bc1
fizzerle <thomas.schallert94@gmail.com>
2019-06-09 12:11:18 +0200

    test configuration

--- apigateway/src/main/java/tuwien/dse/apigateway/cache/RedisService.java
+++ apigateway/src/main/java/tuwien/dse/apigateway/cache/RedisService.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
 
 @Service
+@SpringBootConfiguration
 public class RedisService {
 
     //port: 6379
