8493323860d48a1d4ae5b32f57102ab7312205c8
Dominik Scheffknecht <e1426857@student.tuwien.ac.at>
2019-06-09 15:44:23 +0200

    hystrix between eventservice and entityservice

--- event-storage-service/src/main/java/tuwien/dse/eventstorageservice/EventstorageserviceApplication.java
+++ event-storage-service/src/main/java/tuwien/dse/eventstorageservice/EventstorageserviceApplication.java
@@ -4,12 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableSwagger2
+@EnableCircuitBreaker
 public class EventstorageserviceApplication {
 
 
