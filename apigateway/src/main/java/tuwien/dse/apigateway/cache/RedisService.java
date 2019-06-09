4da352ae53cb0d3aed7e44131d76e8effad070b4
fizzerle <thomas.schallert94@gmail.com>
2019-06-09 12:59:24 +0200

    constructor injection finished

--- apigateway/src/main/java/tuwien/dse/apigateway/cache/RedisService.java
+++ apigateway/src/main/java/tuwien/dse/apigateway/cache/RedisService.java
@@ -4,14 +4,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
 
 @Service
-@SpringBootConfiguration
 public class RedisService {
 
     //port: 6379
@@ -21,18 +23,9 @@ public class RedisService {
     private static final Logger LOGGER = LoggerFactory.getLogger(RedisService.class);
 
     private Jedis jedis;
-    private ObjectMapper mapper = new ObjectMapper();
 
-    @Value("${redis.host}")
-    private String host;
-
-    @Value("${redis.port}")
-    private Integer port;
-
-    @Value("${redis.password}")
-    private String password;
-
-    public RedisService() {
+    @Autowired
+    public RedisService(@Value("${redis.host}") String host,@Value("${redis.port}") Integer port,@Value("${redis.password}") String password) {
         try {
             LOGGER.error("host {} ----------", host);
             LOGGER.error("port {} ----------", host);
